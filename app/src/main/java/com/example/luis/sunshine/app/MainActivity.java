cc24606b0ac4a30c406cc6828fed7cdf7fd981cf
Luis Marquez <gunclub84@gmail.com>
2015-03-08 18:06:19 -0400

    added networking code, made new class for network thread

--- app/src/main/java/com/example/luis/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/luis/sunshine/app/MainActivity.java
@@ -1,19 +1,9 @@
 package com.example.luis.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -52,40 +42,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-
-            String[] forecastArray = {
-            "Today - Sunny - 88/63",
-            "Tomorrow - Foggy - 70/46",
-            "Weds - Cloudy - 72/63",
-            "Thurs - Rainy - 64/51",
-            "Fri - Foggy - 70/46",
-            "Sat - Sunny - 76/68",
-            };
-
-            ArrayList<String> weatherData = new ArrayList<String>(Arrays.asList(forecastArray));
-            ArrayAdapter<String> weatherDataAdapter = new ArrayAdapter<String>(
-                    //The current context(this fragmetns parent activity)
-                    getActivity(),
-                    //ID of list item layout
-                    R.layout.list_item_forecast,
-                    //ID of the textview to populate
-                    R.id.list_item_forecast_textview,
-                    //weather data
-                    weatherData);
-            return rootView;
-        }
-    }
 }
